c665876b0bedf3e18d85b96c782e114f8a751479
MohammedHarish2004 <clashingharris2004@gmail.com>
2024-06-05 15:26:55 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-import { Button } from 'flowbite-react'
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Movies from './pages/Movies.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Missing from './pages/Missing.jsx'
 
 export default function App() {
   return (
-    <Button className='text-blue-500'>App</Button>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/movies' element={<Movies />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+      <Route path='/sign-in' element={<SignIn />}/>
+      <Route path='/sign-up' element={<SignUp />}/>
+      <Route path='*' element={<Missing />}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
---
+++ client/src/pages/Missing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import missing from '../assets/missing.png'
+
+export default function Missing() {
+  return (
+    <div className='flex flex-col items-center justify-center p-3 mt-10'>
+        <img src={missing} className='' alt="Not Found" />
+        <p className='text-4xl mt-4 text-center font-semibold'>Page  <span className='text-red-600'>not found</span></p>
+    </div>
+  )
+}
