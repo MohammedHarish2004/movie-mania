8250c0960ce9d0e21a13fcedbc3e6e5d7a5cd864
MohammedHarish2004 <clashingharris2004@gmail.com>
2024-06-05 17:00:37 +0530

    create and run mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+console.log(err.message);
+})
 
 const app = express()
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Button, Navbar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
+import { PiFilmReelFill } from "react-icons/pi";
+
 export default function Header() {
     const path = useLocation().pathname
 
@@ -7,26 +9,26 @@ export default function Header() {
     <Navbar  className="bg-black p-3 border-b border-slate-700 ">
       <Navbar.Brand>
         <h1 className="flex gap-1">
-            <span className="bg-gradient-to-r from-red-600 to-red-700 hover:bg-gradient-to-bl p-1 px-4 rounded-lg text-xl font-semibold ">Movie</span>
+            <span className="flex  items-center gap-0.5 bg-gradient-to-r from-yellow-300 to-yellow-400 hover:bg-gradient-to-bl p-1 px-4 rounded-lg text-xl font-bold text-black"><PiFilmReelFill />Movie</span>
             <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Mania</span>
         </h1>
       </Navbar.Brand>
       
       <div className="flex items-center gap-2 md:order-2">
         <Link to='/sign-in'>
-            <button className="bg-transparent border-2 border-red-600 hover:bg-red-600 transition delay-50 p-1.5 px-3 rounded-lg  font-semibold">Sign In</button>
+            <button className="bg-transparent border-2 border-yellow-300 hover:bg-yellow-300 hover:text-black transition delay-50 p-1.5 px-3 rounded-lg  font-semibold">Sign In</button>
         </Link>
         <Navbar.Toggle className="bg-transparent border-none"/>
       </div>
       
-      <Navbar.Collapse className="text-white">
+      <Navbar.Collapse>
           <Link to='/'>
-            <Navbar.Link  active={path === '/'} as={'div'} className={path === '/' ? 'bg-transparent font-medium text-red-600' : ''}>
+            <Navbar.Link  active={path === '/'} as={'div'} className={path === '/' ? 'bg-transparent font-medium text-yellow-300  hover:text-yellow-300' : ''}>
                 <span className={`text-lg ${path === '/' ? 'text-white' : ''}`}>Home</span>
             </Navbar.Link>
           </Link>
           <Link to='/movies'>
-            <Navbar.Link  active={path === '/movies'} as={'div'} className={path === '/movies' ? 'bg-transparent text-red-600' : ''} >
+            <Navbar.Link  active={path === '/movies'} as={'div'} className={path === '/movies' ? 'bg-transparent text-yellow-300 hover:text-yellow-300' : ''} >
                 <span className={`text-lg ${path === '/movies' ? 'text-white' : ''}`}>Movies</span>
             </Navbar.Link>
           </Link>
