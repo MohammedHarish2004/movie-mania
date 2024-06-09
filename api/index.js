7ad761cbabd0790a7cea0c89f897430faa70f36e
MohammedHarish2004 <clashingharris2004@gmail.com>
2024-06-09 17:36:34 +0530

    Testing Deployment

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose'
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
 import genreRouter from './routes/genre.route.js'
-
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 dotenv.config()
 
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
 console.log(err.message);
 })
+
+const __dirname = path.resolve()
 
 const app = express()
 
@@ -28,6 +30,12 @@ app.use(cookieParser())
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 app.use('/api/genre',genreRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode
